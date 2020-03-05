0bb075fe65533b428b5bb65036d2fb577896776e
vosure <vosure1@gmail.com>
2020-03-05 12:50:17 +0300

    initial commit, simple prototype

---
+++ Assets/Scripts/DestroySelf.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Small script for easily destroying an object after a while
+/// </summary>
+public class DestroySelf : MonoBehaviour
+{
+    public float Delay = 3f;
+    //Delay in seconds before destroying the gameobject
+
+    void Start ()
+    {
+        Destroy (gameObject, Delay);
+    }
+}
