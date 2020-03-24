f142b34f8402250786ee4c504912cb029c0895c6
vosure <vosure1@gmail.com>
2020-03-24 23:49:24 +0300

    better start from the beginning, import some standard assets

---
+++ MapGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject blockPrefab; //TODO(vosure): Name environmnet objects and save it somewhere as prefabs
+
+    void Start()
+    {
+        GenerateMap();
+    }
+
+    //NOTE(vosure) Temp
+    const int width = 64;
+    const int height = 64;
+
+    public void GenerateMap()
+    {
+        for (int i = -(width / 2); i < width / 2; i++)
+        {
+            GameObject wallBlock = Instantiate(blockPrefab, new Vector3(i, 0.0f, -32.0f), Quaternion.identity);
+        }
+        for (int i = -(width / 2); i < width / 2; i++)
+        {
+            GameObject wallBlock = Instantiate(blockPrefab, new Vector3(32, 0.0f, i), Quaternion.identity);
+        }
+        for (int i = (width / 2); i > -(width / 2); i++)
+        {
+            GameObject wallBlock = Instantiate(blockPrefab, new Vector3(i, 0.0f, 32.0f), Quaternion.identity);
+        }
+        for (int i = (width / 2); i > -(width / 2); i++)
+        {
+            GameObject wallBlock = Instantiate(blockPrefab, new Vector3(-32.0f, 0.0f, -32.0f), Quaternion.identity);
+        }
+
+    }
+}
